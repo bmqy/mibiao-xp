816e7a7e6f77f05215331f21d76f82d72abed222
bmqy <bmqy@qq.com>
2023-09-06 11:38:56 +0800

    update

--- admin/config/database.php
+++ admin/config/database.php
@@ -7,6 +7,8 @@ $dbname = "${DB_NAME}";
 
 // 创建数据库连接
 $conn = new mysqli($servername, $username, $password, $dbname);
+// 添加此行代码，解决中文乱码问题
+$conn->query("SET NAMES 'UTF8'");
 
 // 检查连接是否成功
 if ($conn->connect_error) {
